3efc8c6685b994b2d753dd7ce821641912228432
Imtiaz <imtiazkazarani@gmail.com>
2026-04-27 10:49:58 +0530

    refactor: extract parse and validate_no_negatives into private methods
    - Extracted delimiter parsing logic into a private parse method
    - Extracted negative number validation into a private validate_no_negatives method
    - Simplified add method to a clear parse, validate, sum pipeline

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,15 +1,25 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
+    nums = parse(numbers)
+    validate_no_negatives(nums)
+    nums.sum
+  end
+
+  private
+
+  def parse(numbers)
     if numbers.start_with?("//")
       delimiter = numbers[2]
       numbers = numbers[4..-1]
-      nums = numbers.split(/#{delimiter}|\n/).map(&:to_i)
+      numbers.split(/#{delimiter}|\n/).map(&:to_i)
     else
-      nums = numbers.split(/,|\n/).map(&:to_i)
+      numbers.split(/,|\n/).map(&:to_i)
     end
+  end
+
+  def validate_no_negatives(nums)
     negatives = nums.select { |n| n < 0 }
     raise "negatives not allowed: #{negatives.join(", ")}" if negatives.any?
-    nums.sum
   end
 end
