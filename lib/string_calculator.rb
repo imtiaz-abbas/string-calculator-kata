bb940f7d5160009ccc08cc41ccc1079337c10b93
Imtiaz <imtiazkazarani@gmail.com>
2026-04-27 10:44:46 +0530

    feat: implement custom delimiter parsing
    - Added check for "//" prefix to detect custom delimiter syntax
    - Extracted delimiter character from position 2 of the string
    - Sliced input to remove the "//[delimiter]\n" header before splitting
    - Split numbers using custom delimiter or newline, then summed results

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,12 @@
 class StringCalculator
   def add(numbers)
-        return 0 if numbers.empty?
-        numbers.split(/,|\n/).map(&:to_i).sum
+    return 0 if numbers.empty?
+    if numbers.start_with?("//")
+      delimiter = numbers[2]
+      numbers = numbers[4..-1]
+      return numbers.split(/#{delimiter}|\n/).map(&:to_i).sum
+    else
+      return numbers.split(/,|\n/).map(&:to_i).sum
+    end
   end
 end
