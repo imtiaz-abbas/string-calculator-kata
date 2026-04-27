f189bc4ddb00b47f48671dd6012795a11bea6667
Imtiaz <imtiazkazarani@gmail.com>
2026-04-27 10:58:03 +0530

    feat: support multiple delimiters in bracket syntax
    - Split header and numbers on first newline for cleaner parsing
    - Extracted all bracket-wrapped delimiters using scan with non-greedy regex
    - Built a combined regex pattern from all escaped delimiters
    - Replaced single delimiter split with multi-delimiter pattern split

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -10,9 +10,10 @@ class StringCalculator
 
   def parse(numbers)
     if numbers.start_with?("//")
-      delimiter = numbers[2] == "[" ? numbers[/\[(.+)\]/, 1] : numbers[2]
-      numbers = numbers[numbers.index("\n") + 1..-1]
-      numbers.split(/#{Regexp.escape(delimiter)}|\n/).map(&:to_i).reject { |n| n > 1000 }
+      header, numbers = numbers[2..].split("\n", 2)
+      delimiters = header.start_with?("[") ? header.scan(/\[(.+?)\]/).flatten : [header]
+      pattern = delimiters.map { |d| Regexp.escape(d) }.join("|")
+      numbers.split(/#{pattern}/).map(&:to_i).reject { |n| n > 1000 }
     else
       numbers.split(/,|\n/).map(&:to_i).reject { |n| n > 1000 }
     end
